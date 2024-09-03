2153395f51e5a4bdbac12d6cf915942617ce6961
Ferenandios <www.terunplay@gmail.com>
2024-09-03 11:24:52 +0300

    Created interface for a State

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,13 @@
+export type inputTypeType = "email" | "password";
+
 export interface IInput {
   id: number;
+  type: inputTypeType;
   title: string;
   placeholder: string;
+}
+
+export interface IState {
+  email: string;
+  password: string;
 }
