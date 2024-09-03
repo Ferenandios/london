536c29df61beebcca8a3ecd72c13e7177c09347d
Ferenandios <www.terunplay@gmail.com>
2024-09-03 11:25:57 +0300

    Created inputs info for a Input-components

--- src/components/Form/Inputs/Inputs.tsx
+++ src/components/Form/Inputs/Inputs.tsx
@@ -1,11 +1,16 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { IInput } from "../../../types/types";
 import Input from "./Input/Input";
 
 const Inputs: FC = (): JSX.Element => {
   const inputList: IInput[] = [
-    { id: 0, title: "Почта", placeholder: "Введите Вашу почту" },
-    { id: 1, title: "Пароль", placeholder: "Введите Ваш пароль" },
+    { id: 0, type: "email", title: "Почта", placeholder: "Введите Вашу почту" },
+    {
+      id: 1,
+      type: "password",
+      title: "Пароль",
+      placeholder: "Введите Ваш пароль",
+    },
   ];
   return (
     <>
