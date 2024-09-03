9b1f6388f4cfd00bfa854066fbe5db76f3c29ded
Ferenandios <www.terunplay@gmail.com>
2024-09-03 11:27:08 +0300

    Added imported styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import Title from "./Title/Title";
 import Inputs from "./Inputs/Inputs";
+import Button from "./Button/Button";
+import styles from "./Form.module.css";
 
 const Form: FC = (): JSX.Element => {
   return (
-    <div className="flex flex-col">
+    <div className={styles.inner}>
       <Title />
       <Inputs />
+      <Button />
     </div>
   );
 };
