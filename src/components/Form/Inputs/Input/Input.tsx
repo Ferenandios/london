9d007a0dfd761c1a276abc09f21cbef7867cf44e
Ferenandios <www.terunplay@gmail.com>
2024-09-03 11:26:25 +0300

    Added handleChange function

--- src/components/Form/Inputs/Input/Input.tsx
+++ src/components/Form/Inputs/Input/Input.tsx
@@ -1,12 +1,34 @@
-import React, { FC } from "react";
+import { ChangeEvent, FC } from "react";
+import { setEmail, setPassword } from "../../../../features/global.slice";
+import { useAppDispatch, useAppSelector } from "../../../../hooks";
 import { IInput } from "../../../../types/types";
 
 const Input: FC<{ input: IInput }> = ({ input }): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const value = useAppSelector((state) => state[input.type]);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    switch (input.type) {
+      case "email":
+        dispatch(setEmail(event.target.value));
+        break;
+      case "password":
+        dispatch(setPassword(event.target.value));
+        break;
+      default:
+        alert(52);
+        break;
+    }
+  };
   return (
     <>
-      <div>
+      <div className="flex">
         <h3>{input.title}</h3>
-        <input placeholder={input.placeholder} type="text" />
+        <input
+          onChange={(event) => handleChange(event)}
+          value={value}
+          placeholder={input.placeholder}
+          type="text"
+        />
       </div>
     </>
   );
