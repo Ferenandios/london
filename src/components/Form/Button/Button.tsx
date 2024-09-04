a674edfd1c72dd6ec855c7e6a8d9bec9830d617d
Ferenandios <www.terunplay@gmail.com>
2024-09-04 19:46:34 +0300

    Added mock

--- src/components/Form/Button/Button.tsx
+++ src/components/Form/Button/Button.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
+import { useAppSelector } from "../../../hooks";
 
 const Button: FC = (): JSX.Element => {
+  const { email, password } = useAppSelector((state) => state);
+  const handleClick = () => {
+    fetch("");
+    alert(
+      `Data sended succesfully! \n\nEmail: ${email}\nPassword: ${password}`
+    );
+  };
   return (
     <>
       <div>
-        <button className="text-white bg-lime-300 px-[45px] py-[15px] rounded-full">
+        <button
+          onClick={handleClick}
+          className="text-white bg-lime-300 px-[45px] py-[15px] rounded-full"
+        >
           Отправить
         </button>
       </div>
